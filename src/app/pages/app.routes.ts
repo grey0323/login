c76de35be8b8a6cf9c66ee9bc31bdd6e0280374d
Greybin Carrasco <carrascogreybin1@gmail.com>
2025-03-09 14:09:22 -0400

    Adding guard

--- src/app/pages/app.routes.ts
+++ src/app/pages/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { authGuard } from '../custom/auth.guard';
 
 export const routes: Routes = [
 
     {path:"", loadComponent: ()=> import("../pages/login/login.component")},
     {path:"registro", loadComponent: ()=> import("../pages/registro/registro.component")},
-    {path:"inicio", loadComponent: ()=> import("../pages/inicio/inicio.component")}
+    {path:"inicio", loadComponent: ()=> import("../pages/inicio/inicio.component"), canActivate:[authGuard]}
     
 ];
